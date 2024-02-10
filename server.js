fbaa128a34e2b15c4c85ed3cd87a3fe92c71c24c
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-02-10 20:35:23 +0000

    Add middleware chain for /chain route

--- server.js
+++ server.js
@@ -28,6 +28,21 @@ app.get(
     },
 );
 
+const one = (req, res, next) => {
+    console.log('one');
+    next();
+};
+const two = (req, res, next) => {
+    console.log('two');
+    next();
+};
+const three = (req, res) => {
+    console.log('three');
+    res.send('Finished!');
+};
+
+app.get('/chain(.html)?', [one, two, three]);
+
 app.get('*', (req, res) => {
     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
