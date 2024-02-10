269eb65b7d170eb1ec5a2c4bdf7c8e1f6d5c67da
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-02-10 20:03:17 +0000

    Add routes for index.html and new-page.html

--- server.js
+++ server.js
@@ -3,8 +3,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.get('/', (req, res) => {
+app.get('^/$|/index(.html)?', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.get('/new-page(.html)?', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
