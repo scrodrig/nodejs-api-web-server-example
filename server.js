88d78731dee9c2126b190f7f8f6251649526be89
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-02-10 20:06:27 +0000

    Add redirect from old-page to new-page

--- server.js
+++ server.js
@@ -11,4 +11,9 @@ app.get('/new-page(.html)?', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
 });
 
+app.get('/old-page(.html)?', (req, res) => {
+    res.redirect(301, '/new-page.html');
+});
+
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
