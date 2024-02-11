9531d78b6dff689fdb81d5b8e57036faae340242
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-02-11 23:48:51 +0000

    Refactor employee routes to use controller functions

--- routes/api/employees.js
+++ routes/api/employees.js
@@ -1,36 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const path = require('path');
-const data = {};
-data.employees = require('../../data/employees.json');
+const {
+    getAllEmployees,
+    getEmployee,
+    createNewEmployee,
+    updateEmployee,
+    deleteEmployee,
+} = require('../../controllers/employeesController');
 
 router
     .route('/')
-    .get((req, res) => {
-        res.json(data.employees);
-    })
-    .post((req, res) => {
-        res.json({
-            firstName: req.body.firstName,
-            lastName: req.body.lastName,
-        });
-    })
-    .put((req, res) => {
-        res.json({
-            id: req.body.id,
-            firstName: req.body.firstName,
-        });
-    })
-    .delete((req, res) => {
-        res.json({
-            id: req.body.id,
-        });
-    });
+    .get(getAllEmployees)
+    .post(createNewEmployee)
+    .put(updateEmployee)
+    .delete(deleteEmployee);
 
-router.route('/:id').get((req, res) => {
-    res.json({
-        id: req.params.id,
-    });
-});
+router.route('/:id').get(getEmployee);
 
 module.exports = router;
