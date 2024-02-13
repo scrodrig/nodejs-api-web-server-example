0f4e7ccf8fa3f4b4819a76c89a77a1e593beb21f
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-02-13 17:43:14 +0000

    Add registerController.js with handleNewUser function

---
+++ controllers/registerController.js
@@ -0,0 +1,42 @@
+const usersDB = {
+    users: require('../model/users'),
+    setUsers: (payload) => {
+        usersDB.users = payload;
+    },
+};
+
+const fsPromises = require('fs').promises;
+const path = require('path');
+const bcrypt = require('bcrypt');
+
+const handleNewUser = async (req, res) => {
+    const { user, pwd } = req.body;
+    if (!user || !pwd) {
+        return res.status(400).json({ error: 'Include username and password' });
+    }
+    const duplicated = usersDB.users.find((person) => person.user === user);
+    if (duplicated) {
+        return res.sendStatus(409);
+    }
+
+    try {
+        const hashedPwd = await bcrypt.hash(pwd, 10);
+        //Store the new user in the database
+        const newUser = {
+            username: user,
+            password: hashedPwd,
+        };
+        usersDB.setUsers([...usersDB.users, newUser]);
+
+        await fsPromises.writeFile(
+            path.join(__dirname, '..', 'models', 'users.json'),
+            JSON.stringify(usersDB.users),
+        );
+        console.log(usersDB.users);
+        res.status(201).json({ success: `New user ${user} created` });
+    } catch (error) {
+        res.status(500).json({ message: 'Internal server error' });
+    }
+};
+
+module.exports = { handleNewUser };
