7a2e720d2f3d3c9f15ad580e311a0fcab9c95b1b
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-02-13 17:48:29 +0000

    Fix path to users.json file

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -29,7 +29,7 @@ const handleNewUser = async (req, res) => {
         usersDB.setUsers([...usersDB.users, newUser]);
 
         await fsPromises.writeFile(
-            path.join(__dirname, '..', 'models', 'users.json'),
+            path.join(__dirname, '..', 'model', 'users.json'),
             JSON.stringify(usersDB.users),
         );
         console.log(usersDB.users);
