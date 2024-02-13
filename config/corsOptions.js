d16a196cec21c2131f6f561d6841e6b12f731cee
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-02-13 19:45:26 +0000

    Refactor corsOptions.js to use whiteList module

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,9 +1,5 @@
-const whitelist = [
-    'https://yoursite.com',
-    'https://www.google.com',
-    'http://localhost:3500',
-    'http://127.0.0.1:5500',
-];
+const whitelist = require('./whiteList');
+
 const corsOptions = {
     origin: (origin, callback) => {
         if (whitelist.indexOf(origin) !== -1 || !origin) {
