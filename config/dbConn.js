2510a83c3cbf2bd8c504ab24e98c377ab0d41c77
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-02-15 23:49:53 +0000

    Add database connection configuration

---
+++ config/dbConn.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.DATABASE_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+module.exports = connectDB;
