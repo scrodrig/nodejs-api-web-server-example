89ca9d894a1c5ac63bc58d3e3d911222aa56c764
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-02-15 16:01:06 +0000

    Refactor access token generation to include user roles

--- controllers/authController.js
+++ controllers/authController.js
@@ -23,8 +23,14 @@ const handleLogin = async (req, res) => {
     const match = await bcrypt.compare(pwd, foundUser.password);
 
     if (match) {
+        const roles = Object.values(foundUser.roles);
         const accessToken = jwt.sign(
-            { username: foundUser.username },
+            {
+                UserInfo: {
+                    username: foundUser.username,
+                    roles,
+                },
+            },
             process.env.ACCESS_TOKEN_SECRET,
             { expiresIn: '30s' },
         );
