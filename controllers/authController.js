96f2dd7b4386554ad77f79c2082908c9d5e4e88b
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-02-16 01:04:57 +0000

    Refactor authentication logic and update User model

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,21 +1,13 @@
-const usersDB = {
-    users: require('../model/users'),
-    setUsers: (payload) => {
-        usersDB.users = payload;
-    },
-};
+const User = require('../model/User');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-require('dotenv').config();
-const fsPromises = require('fs').promises;
-const path = require('path');
 
 const handleLogin = async (req, res) => {
     const { user, pwd } = req.body;
     if (!user || !pwd) {
         return res.status(400).json({ error: 'Include username and password' });
     }
-    const foundUser = usersDB.users.find((person) => person.username === user);
+    const foundUser = await User.findOne({ username: user }).exec();
     if (!foundUser) {
         return res.sendStatus(401);
     }
@@ -40,17 +32,10 @@ const handleLogin = async (req, res) => {
             { expiresIn: '1d' },
         );
 
-        //! This is just for not having a database saving the refresh tokens
-        const otherUsers = usersDB.users.filter(
-            (person) => person.username !== foundUser.username,
-        );
-        const currentUser = { ...foundUser, refreshToken };
-        usersDB.setUsers([...otherUsers, currentUser]);
+        foundUser.refreshToken = refreshToken;
+        await foundUser.save();
+        
 
-        await fsPromises.writeFile(
-            path.join(__dirname, '..', 'model', 'users.json'),
-            JSON.stringify(usersDB.users),
-        );
         res.cookie('jwt', refreshToken, {
             httpOnly: true,
             // sameSite: 'None', //This is for the cookie to be sent in a cross-origin request for develoopment
