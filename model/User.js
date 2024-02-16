7b3e87bd6139b6787c0c034fac028c4f548fa3be
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-02-16 00:22:42 +0000

    Add User model definition

---
+++ model/User.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+    },
+    roles: {
+        Admin: Number,
+        Editor: Number,
+        User: {
+            type: Number,
+            default: 2001,
+        },
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    refreshedToken: {
+        type: String,
+    }
+});
+
+module.exports = mongoose.model('User', userSchema);
